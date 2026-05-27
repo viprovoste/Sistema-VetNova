7049491d11c7dfc7e1a15560cb6c08e63dc55d1c
Vicente <vi.provoste@duocuc.cl>
2026-05-26 22:38:42 -0400

    Creacion de Repositories

--- agendamiento_sucursales/src/main/java/com/vetnova/agendamiento_sucursales/repository/BoxRepository.java
+++ agendamiento_sucursales/src/main/java/com/vetnova/agendamiento_sucursales/repository/BoxRepository.java
@@ -1,5 +1,16 @@
 package com.vetnova.agendamiento_sucursales.repository;
 
-public class BoxRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vetnova.agendamiento_sucursales.model.Box;
+import com.vetnova.agendamiento_sucursales.model.EstadoBox;
+
+@Repository
+public interface BoxRepository extends JpaRepository<Box, Long> {
+    List<Box> findBySucursalIdSucursal(Long idSucursal);
+    List<Box> findBySucursalIdSucursalAndEstado(Long idSucursal, EstadoBox estado);
     
 }
