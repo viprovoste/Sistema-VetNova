de0dea1c840ac3bbfc0fc961514b0afa657c6f12
sebastian <sebast.mirandam@duocuc.cl>
2026-06-26 21:11:42 -0400

    api swagger integrada

---
+++ notificaciones/src/test/java/com/vetnova/notificaciones/NotificacionesApplicationTests.java
@@ -0,0 +1,11 @@
+package com.vetnova.notificaciones;
+
+import org.junit.jupiter.api.Test;
+
+class NotificacionesApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Test vacío - solo verifica que el contexto del test carga
+    }
+}
